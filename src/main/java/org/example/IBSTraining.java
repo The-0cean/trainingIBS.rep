09022517bf48b48d9592c459996e8ea4ca9b7751
The-0cean <maksimstrig@gmail.com>
2023-09-19 16:59:53 +0300

    projectIBS

---
+++ src/main/java/org/example/IBSTraining.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class IBSTraining {
+
+        String info1 = "Как сас";
+        int info2 = 12;
+        double info3 = 3.14;
+
+
+
+
+}
---
+++ src/main/java/org/example/lol.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class lol {
+    public static void main(String[] args) {
+        String name = "IVAN";
+        int age = 20;
+
+        String lastName;
+        System.out.println(age);
+    }
+}
+
+
